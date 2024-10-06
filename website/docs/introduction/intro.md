36e0f56e4aa7a9c7d350ea29cd1796f12d2456dc
Ratnopam <ratnopam@MacBookAir.attlocal.net>
2024-10-06 00:08:48 -0500

    add website document initial structure

---
+++ website/docs/introduction/intro.md
@@ -0,0 +1,6 @@
+---
+sidebar_position: 1
+sidebar_label: What is Spark RAPIDS
+---
+
+# Introduction to SPARK RAPIDS
--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,29 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Enable GPU-accelerated data science workflows',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        RAPIDS, a project developed by NVIDIA, comprises a suite of open-source libraries that are hinged upon CUDA, thereby enabling GPU-accelerated data science workflows
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Experience unparralel performance boost in your data processing workflows',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Experience a performance boost in data preparation tasks, allowing you to transition quickly to the subsequent stages of your pipeline..
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by NVIDIA GPU Architecture',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        A transformative parallel computing platform designed for enhancing computational processes on NVIDIA's GPU architecture.
       </>
     ),
   },
